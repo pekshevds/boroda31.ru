79b53705dda2937d8d1731a96992646805311ae1
MaximPekshev <mp198769@gmail.com>
2020-12-28 14:25:16 +0300

    commit after deploy

---
+++ passenger_wsgi.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import os, sys
+sys.path.insert(0, '/var/www/u0872810/data/www/boroda31.ru/boroda31')
+sys.path.insert(1, '/var/www/u0872810/data/boroda_env/lib/python3.9/site-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'boroda31.settings'
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
